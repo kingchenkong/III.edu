719d827858e6eae6e1268aa52cd97ae1fd8f7c0d
陳維漢 <wikeyno@gmail.com>
2016-06-02 12:16:33 +0800

    Hw_0602 PHP

---
+++ 0601_PHP&MySQL/Hw/ex04_03.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+     <meta charset='utf-8'>
+     <title>練習ex04_03</title>
+     <style>
+     tbody tr:nth-child(2n) {background-color:#CAFFFF;}
+			tbody tr:nth-child(2n+1) {background-color:#DEFFAC;}
+     </style>
+  </head>
+  <body>
+    <?php
+    	echo "<h1> 九 九 乘 法 表 </h1>";
+       echo "<table border='1'><tr>";
+       for ($y = 1 ; $y <= 9 ; $y++  ) {
+       	for ($x = 1 ; $x <= 9 ; $x++  ) {
+       		echo "<td>  $x x $y =&nbsp". $y*$x . "</td>";
+       	}
+       	echo "</tr>";
+       }
+       echo "</table>";
+    ?>
+   </body> 
+</html>
